97387d01ff6c6fb2068793fa8661b645a8a29fc8
BSH7 <gamaugi77@naver.com>
2022-02-25 18:57:44 +0900

    feat : jwt 검증 filter 및 refresh token으로 재발급 api 추가

--- vlog-webservice-server/src/main/java/com/maen/vlogwebserviceserver/config/auth/SecurityConfig.java
+++ vlog-webservice-server/src/main/java/com/maen/vlogwebserviceserver/config/auth/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.maen.vlogwebserviceserver.config.auth;
 
+import com.maen.vlogwebserviceserver.config.auth.filter.JwtAuthFilter;
 import com.maen.vlogwebserviceserver.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @Configuration
@@ -20,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic().disable()
                 .formLogin().disable()
                 .cors()
-//                .configurationSource(corsConfigurationSource())
+                .configurationSource(corsConfigurationSource())
                 .and()
                 .csrf().disable()
                 .headers().frameOptions().disable()
@@ -28,28 +34,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                     .authorizeRequests()
-                        .antMatchers("/**", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
-             ;
-//                    .oauth2Login()
-//                //.loginPage("/api/v1/token/expired")
-//                        .successHandler(successHandler)
-//                        .userInfoEndpoint().userService(oAuth2UserService);
-
-//        http.addFilterBefore(new JwtAuthFilter(tokenService, userRepository), UsernamePasswordAuthenticationFilter.class);
+                        .antMatchers("/api/**", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                        .anyRequest().authenticated();
+//                .and()
+//                    .oauth2Login().loginPage("/api/v1/token/expired");
+        http.addFilterBefore(new JwtAuthFilter(tokenService, userRepository), UsernamePasswordAuthenticationFilter.class);
     }
 
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//
-//        configuration.addAllowedOrigin("http://localhost:3000");
-//        configuration.addAllowedHeader("*");
-//        configuration.addAllowedMethod("*");
-//        configuration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
--- vlog-webservice-server/src/main/java/com/maen/vlogwebserviceserver/web/LoginApiController.java
+++ vlog-webservice-server/src/main/java/com/maen/vlogwebserviceserver/web/LoginApiController.java
@@ -1,6 +1,8 @@
 package com.maen.vlogwebserviceserver.web;
 
 
+import com.maen.vlogwebserviceserver.config.auth.JwtService;
+import com.maen.vlogwebserviceserver.config.auth.dto.Jwt;
 import com.maen.vlogwebserviceserver.service.user.LoginService;
 import com.maen.vlogwebserviceserver.web.dto.LoginResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -10,11 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RequiredArgsConstructor
 @Controller
 public class LoginApiController {
 
     private final LoginService loginService;
+    private final JwtService tokenService;
 
     @GetMapping("/api/v1/login/{provider}")
     public String login(@PathVariable String provider) {
@@ -26,6 +32,28 @@ public class LoginApiController {
     public ResponseEntity<LoginResponseDto> getToken(@PathVariable String provider, @RequestParam String code) {
         LoginResponseDto responseDto = loginService.login(provider, code);
         return ResponseEntity.ok().body(responseDto);
+    }
+
+    @GetMapping("/api/v1/jwt/expired")
+    public String auth() {
+        throw new RuntimeException();
+    }
+
+    @GetMapping("/api/v1/jwt/refresh")
+    public String refreshAuth(HttpServletRequest request, HttpServletResponse response) {
+        String token = request.getHeader("REFRESH_TOKEN");
+
+        if (token != null && tokenService.verifyToken(token)) {
+            String userId = tokenService.getPayload(token);
+            Jwt newToken = tokenService.generateToken(userId);
+
+            response.addHeader("Auth", newToken.getAccessToken());
+            response.addHeader("Refresh", newToken.getRefreshToken());
+            response.setContentType("application/json;charset=UTF-8");
+
+            return "REFRESH COMPLETE";
+        }
+        throw new RuntimeException();
     }
 
 }
